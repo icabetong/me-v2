f3f54c39889015d18d31c03da088f850ae3d8b55
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2022-06-18 18:07:29 +0800

    more changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { MetaHeadEmbed } from '@phntms/react-share'
+import MetaHeadEmbed from '@phntms/react-share/lib/components/MetaHeadEmbed'
 
 import Navigation from '../components/navigation/Navigation'
 
