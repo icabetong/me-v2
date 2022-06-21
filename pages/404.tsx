9255b0b808e55c3c3861979df964db0c16374530
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2022-06-21 17:04:22 +0800

    tweaked 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -11,20 +11,19 @@ const NotFound: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{t("header")}</title>
+        <title>{t("title")}</title>
       </Head>
-      <div className="flex flex-col md:flex-row items-center justify-center w-screen h-screen bg-russian text-azureish-white font-inter px-12">
-        <h1 className="text-9xl mb-8">404</h1>
-        <div className="text-lg max-w-screen-md px-0 md:px-8 flex flex-col items-start justify-between">
-          <h2 className="text-3xl font-semibold mb-1">{t("header")}</h2>
-          <h3 className="text-gray-400">{t("summary")}</h3>
+      <div className="flex flex-col md:flex-row items-center justify-center w-screen h-screen bg-russian text-azureish-white font-inter px-8 md:px-12">
+        <h1 className="text-8xl md:text-9xl mb-8">404</h1>
+        <div className="max-w-screen-md px-0 md:px-8 flex flex-col items-start justify-between">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-2">{t("header")}</h2>
+          <h3 className="text-md md:text-lg text-gray-400">{t("summary")}</h3>
 
           <motion.div 
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9, x: "-2px", y: "2px" }}
-            className="my-8 cursor-pointer">
+            whileHover={{ scale: 1.1 }} 
+            className="my-8 h-fit">
             <Link href="/">
-              <a className="bg-navajo-white text-russian font-medium px-8 py-2 rounded-full">{t("back-to-home")}</a>
+              <a className="bg-navajo-white text-russian font-medium px-4 md:px-8 py-2 rounded-full">{t("back-to-home")}</a>
             </Link>
           </motion.div>
         </div>
